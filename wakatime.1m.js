18d303cdb12d8decfa37e1c386d15a3bf096a5dd
Amir <cantoamir@gmail.com>
2020-05-22 00:07:19 -0500

    validations for no data

--- wakatime.1m.js
+++ wakatime.1m.js
@@ -19,6 +19,8 @@ let currentDate =  new Date().toLocaleString().slice(0,10).replace(/,/g,'');
 
 const icon  = '/9j/2wCEAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQECAgICAgICAgICAgMDAwMDAwMDAwMBAQEBAQEBAgEBAgICAQICAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA//dAAQAAv/uAA5BZG9iZQBkwAAAAAH/wAARCAAQABADABEAAREBAhEB/8QAWAABAQEAAAAAAAAAAAAAAAAACAAFAQEAAAAAAAAAAAAAAAAAAAAAEAABBAMBAAMBAAAAAAAAAAAHAwQFBgIICQETFxglEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMAAAERAhEAPwB+h8bU7uA5ltsN6zNcWOgBQOhK1p53aQD8uW8MDowxI1m7JGyRZNdmpz6AmySXig7Hcq5h4RV23wjm8e7TbKuPFEksQwS3U6PxHnZTZrQ0hmD8GAIuhcD9FNOyZdyCWwTTIA0WJpDNCNrHfby6nJeAPQTkbbEL2eEj3btFTGYYtnSyGaThuoH/0HdSreOuPQ6NnOro6DrjbeV9hIBNJmqO0lJE91MIZhRSSyB9kyGvGx1aHjSRuA1JAsvkh46h5hPH+nl6oq3xbpt8MlQiBdh/2PGoc5v84gfbqlyviriML5thtPbBPdQgGVw8MCCkSI3XnWauXdlDW2+EslXiJ8cScv4n57GZfEotiumuoqgH/9k=';
 let url = 'https://wakatime.com/api/v1/users/current/summaries?api_key='+  WAKATIME_API_KEY+'&start='+ currentDate + '&end='+ currentDate;
+
+
 https.get(url,(res) => {
     let body = "";
 
@@ -35,15 +37,23 @@ https.get(url,(res) => {
 
                 let categories = wakaData.data[0].categories || undefined;
                 if(categories){
-         
-                    console.log(categories[0].text + ' | image='+icon);
+                    if(categories[0].text){
+                        console.log(categories[0].text + ' | image='+icon);
+                    } else {
+                        console.log('00:00:00 hrs | image='+icon);
+                    }
+                   
+                }   else {
+                    console.log('00:00:00 hrs | image='+icon);
                 }
                 
 
+            } else {
+                console.log('00:00:00 hrs | image='+icon);
             }
             // do something with JSON
         } catch (error) {
-            console.log('Wakatime');
+            console.log('00:00:00 hrs | image='+icon);
         };
     });
 
