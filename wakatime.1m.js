f7dd37e26935764bf88f8d73ba948118995d711b
Amir <cantoamir@gmail.com>
2020-05-21 23:35:09 -0500

    Fixed problem of UTC time and added wakatime icon to the bar

--- wakatime.1m.js
+++ wakatime.1m.js
@@ -5,7 +5,7 @@
  * <bitbar.version>v1.0.0</bitbar.version>
  * <bitbar.author>Amir Canto</bitbar.author>
  * <bitbar.author.github>amircp</bitbar.author.github>
- * <bitbar.image>https://imgur.com/a/ISrsuM3</bitbar.image>
+ * <bitbar.image>https://imgur.com/a/gFijsck</bitbar.image>
  * <bitbar.desc>Wakatime plugin for Bitbar, check your coding activity from your bar.</bitbar.desc>
  * <bitbar.dependencies>node</bitbar.dependencies>
  *
@@ -15,10 +15,9 @@ const WAKATIME_API_KEY = ''; // Get your API Key here: https://wakatime.com/sett
 const https = require('https');
 
 
-let currentDate =  new Date().toISOString().slice(0,10);
+let currentDate =  new Date().toLocaleString().slice(0,10).replace(/,/g,'');
 
-
-
+const icon  = '/9j/2wCEAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQECAgICAgICAgICAgMDAwMDAwMDAwMBAQEBAQEBAgEBAgICAQICAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA//dAAQAAv/uAA5BZG9iZQBkwAAAAAH/wAARCAAQABADABEAAREBAhEB/8QAWAABAQEAAAAAAAAAAAAAAAAACAAFAQEAAAAAAAAAAAAAAAAAAAAAEAABBAMBAAMBAAAAAAAAAAAHAwQFBgIICQETFxglEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMAAAERAhEAPwB+h8bU7uA5ltsN6zNcWOgBQOhK1p53aQD8uW8MDowxI1m7JGyRZNdmpz6AmySXig7Hcq5h4RV23wjm8e7TbKuPFEksQwS3U6PxHnZTZrQ0hmD8GAIuhcD9FNOyZdyCWwTTIA0WJpDNCNrHfby6nJeAPQTkbbEL2eEj3btFTGYYtnSyGaThuoH/0HdSreOuPQ6NnOro6DrjbeV9hIBNJmqO0lJE91MIZhRSSyB9kyGvGx1aHjSRuA1JAsvkh46h5hPH+nl6oq3xbpt8MlQiBdh/2PGoc5v84gfbqlyviriML5thtPbBPdQgGVw8MCCkSI3XnWauXdlDW2+EslXiJ8cScv4n57GZfEotiumuoqgH/9k=';
 let url = 'https://wakatime.com/api/v1/users/current/summaries?api_key='+  WAKATIME_API_KEY+'&start='+ currentDate + '&end='+ currentDate;
 https.get(url,(res) => {
     let body = "";
@@ -36,16 +35,15 @@ https.get(url,(res) => {
 
                 let categories = wakaData.data[0].categories || undefined;
                 if(categories){
-                    console.log('Wakatime');
-                    console.log('---');
-                    console.log('Coding Activity: ' + categories[0].digital + ' hrs');
+         
+                    console.log(categories[0].text + ' | image='+icon);
                 }
                 
 
             }
             // do something with JSON
         } catch (error) {
-            console.error(error.message);
+            console.log('Wakatime');
         };
     });
 
